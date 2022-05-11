4909c4256da35907b5b6e306da5de3a8088bcc17
Meatisdelicious <ssquerel@gmail.com>
2022-05-11 17:27:56 +0200

    test github2

---
+++ Metar-taf-translator.js
@@ -0,0 +1,16 @@
+// Traducteur Metar et TAF en language parle
+
+console.log("hello world wasaaaaa");
+
+// dot.env permet de charger des valeurs depuis mon environnement
+//
+// xo, gulp, mocha, test unitaires, git, gitub action
+// programmation fonctionnelle uniquement
+
+/*
+1. git add .
+2. git commit -m 'your msg'
+3. git push
+
+xo, gulp doesn't work on gitubaction, mocha
+ */
