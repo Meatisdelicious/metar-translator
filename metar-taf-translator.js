4d6a8f1cc7a6200c46be5e7907c66c29ce4bfb7c
Meatisdelicious <ssquerel@gmail.com>
2022-05-13 16:36:32 +0200

    metar-->codeOaci-->phraseHumain

--- metar-taf-translator.js
+++ metar-taf-translator.js
@@ -2,15 +2,31 @@
 
 // METAR: LFPN 101400Z AUTO 22015KT 170V250 CAVOK 28/12 Q1017 TEMPO 22012G22KT=
 
+import * as R from 'ramda';
+import fs from 'fs-extra';
+
+// StringToCodeOaci
+const regexp = /^LF[A-Z]{2}/;
+const stringtoCodeOaci = (string) => regexp.exec(string);
+const getFromRegex = (fn) => R.pipe(fn, R.head); // Prd que la tete du execs car il retourne plusieurs trucs
+const logMethod = (string) => R.pipe(R.concat(string)); // Tap retourne exactement la mm valeur que l'entrer de facon a pouvoir mettre des console.log()
+
+const recupCodeOaci = (string) =>
+  R.pipe(
+    getFromRegex(stringtoCodeOaci),
+    logMethod('Metar de l aerodrome ')
+  )(string);
+
 // String to file json
-const R = require('ramda');
-const fs = require('fs-extra');
 
 const stringToJson = JSON.stringify;
-const writeJson = (file) => (data) => fs.writeJson(file, data);
-
-const jsonToJsonFile = R.pipe(stringToJson, writeJson('metar.json'));
+const writeJson1 = (file) => (data) => fs.writeJson(file, data);
+const jsonToJsonFile = R.pipe(stringToJson, writeJson1('metar.json'));
 
 console.log(
-  jsonToJsonFile('LFPN 101400Z AUTO 22015KT 170V250 CAVOK 28/12 Q1017 TEMPO')
+  jsonToJsonFile(
+    recupCodeOaci(
+      'LFPN 101400Z AUTO 22015KT 170V250 CAVOK 28/12 Q1017 TEMPO 22012G22KT='
+    )
+  )
 );
