c2c47bccca6bbedc67a8411f804073b544f763f0
Meatisdelicious <ssquerel@gmail.com>
2022-05-16 17:49:54 +0200

    autres modifs + fc tempe ambiante

--- metar-taf-translator.js
+++ metar-taf-translator.js
@@ -5,45 +5,53 @@
 import * as R from 'ramda';
 import fs from 'fs-extra';
 
-/*
-
-// En fonctionnel : Pression
-import * as R from 'ramda';
-const regexp = /Q[0-9]{4}/gim;
-const recupPression = (str) => regexp.exec(str);
-const getFromRegex = (fn) => R.pipe(fn, R.head); //prd que la tete du execs car il retourne plusieurs trucs
-const logMethod = (string) => R.tap(R.pipe(R.concat(string), console.log)); //tap retourne exactement la mm valeur que l'entrer de facon a pouvoir mettre des console.log()
+// En fonctionnel : Temperature ambiante
+// import * as R from 'ramda';
+const regexp4 = /[0-9]{2}\//gi;
+const recupTempeAmb = (str) => regexp4.exec(str);
+const getFromRegex4 = (fn) => R.pipe(fn, R.head); //prd que la tete du execs car il retourne plusieurs trucs
+const logMethod4 = (string) => R.tap(R.pipe(R.concat(string), console.log)); //tap retourne exactement la mm valeur que l'entrer de facon a pouvoir mettre des console.log()
 
 R.pipe(
-  getFromRegex(recupPression),
-  logMethod('Pression (Hpa) de ')
+  getFromRegex4(recupTempeAmb),
+  logMethod4('Temperature ambiante (°C) de : ')
+)('LFPX 101400Z AUTO 22015KT 170V250 CAVOK 28/12 Q1017 TEMPO 22012G22KT=');
+
+// En fonctionnel : Pression
+// import * as R from 'ramda';
+const regexp3 = /Q[0-9]{4}/gim;
+const recupPression = (str) => regexp3.exec(str);
+const getFromRegex3 = (fn) => R.pipe(fn, R.head); //prd que la tete du execs car il retourne plusieurs trucs
+const logMethod3 = (string) => R.tap(R.pipe(R.concat(string), console.log)); //tap retourne exactement la mm valeur que l'entrer de facon a pouvoir mettre des console.log()
+
+R.pipe(
+  getFromRegex3(recupPression),
+  logMethod3('Pression (Hpa) de ')
 )('LFPX 101400Z AUTO 22015KT 170V250 CAVOK 28/12 Q1017 TEMPO 22012G22KT=');
 
 // En fonctionnel : Type de metar
-import * as R from 'ramda';
-const regexp = /A[A-Z]{3}/gim;
-const recupTypeMetar = (str) => regexp.exec(str);
-const getFromRegex = (fn) => R.pipe(fn, R.head); //prd que la tete du execs car il retourne plusieurs trucs
-const logMethod = (string) => R.tap(R.pipe(R.concat(string), console.log)); //tap retourne exactement la mm valeur que l'entrer de facon a pouvoir mettre des console.log()
+// import * as R from 'ramda';
+const regexp2 = /A[A-Z]{3}/gim;
+const recupTypeMetar = (str) => regexp2.exec(str);
+const getFromRegex2 = (fn) => R.pipe(fn, R.head); //prd que la tete du execs car il retourne plusieurs trucs
+const logMethod2 = (string) => R.tap(R.pipe(R.concat(string), console.log)); //tap retourne exactement la mm valeur que l'entrer de facon a pouvoir mettre des console.log()
 
 R.pipe(
-    getFromRegex(recupTypeMetar),
-    logMethod(' de type : ')
+  getFromRegex2(recupTypeMetar),
+  logMethod2(' de type : ')
 )('LFPX 101400Z AUTO 22015KT 170V250 CAVOK 28/12 Q1017 TEMPO 22012G22KT=');
-
-*/
 
 // StringToCodeOaci
 
-const regexp = /^LF[A-Z]{2}/;
-const stringtoCodeOaci = (string) => regexp.exec(string);
-const getFromRegex = (fn) => R.pipe(fn, R.head); // Prd que la tete du execs car il retourne plusieurs trucs
-const logMethod = (string) => R.pipe(R.concat(string)); // Tap retourne exactement la mm valeur que l'entrer de facon a pouvoir mettre des console.log()
+const regexp1 = /^LF[A-Z]{2}/;
+const stringtoCodeOaci = (string) => regexp1.exec(string);
+const getFromRegex1 = (fn) => R.pipe(fn, R.head); // Prd que la tete du execs car il retourne plusieurs trucs
+const logMethod1 = (string) => R.pipe(R.concat(string)); // Tap retourne exactement la mm valeur que l'entrer de facon a pouvoir mettre des console.log()
 
 const recupCodeOaci = (string) =>
   R.pipe(
-    getFromRegex(stringtoCodeOaci),
-    logMethod('Metar de l aerodrome ')
+    getFromRegex1(stringtoCodeOaci),
+    logMethod1('Metar de l aerodrome ')
   )(string);
 
 // String to file json
